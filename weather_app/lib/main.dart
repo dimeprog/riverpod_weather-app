6f3a11fa06fa396a4bcbc6df3d585a4c64a5bcb8
dimeprog <dimejiawoyefa5@gmail.com>
2022-11-03 22:42:37 +0100

    completed the weather search app with state Provider

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,104 +13,102 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData.dark(),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cityWeather = ref.watch(weatherProvider);
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        elevation: 0,
+        centerTitle: true,
+        title: const Text('Weather'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          cityWeather.when(
+              data: (data) => Text(
+                    data!,
+                    style: const TextStyle(fontSize: 40),
+                  ),
+              error: (error, _) => const Text('Error'),
+              loading: () => const CircularProgressIndicator()),
+          Expanded(
+            child: ListView.builder(
+                itemCount: City.values.length,
+                itemBuilder: (context, index) {
+                  final curCity = City.values[index];
+                  final isSelected = curCity == ref.watch(currentCityProvider);
+                  return ListTile(
+                    onTap: () {
+                      ref.read(currentCityProvider.notifier).state = curCity;
+                    },
+                    leading: Text(curCity.toString()),
+                    trailing: isSelected ? const Icon(Icons.check) : null,
+                  );
+                }),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+//  type of weather
+typedef WeatherEmoji = String;
+const unKnownWeatherEmoji = '🤷‍♂️';
+// get weather method
+Future<WeatherEmoji> getWeather(City city) async {
+  return Future.delayed(
+    const Duration(seconds: 2),
+    () =>
+        {
+          City.abuja: '🤑',
+          City.ibadan: '😎',
+          City.ife: '😎',
+          City.ilesa: '😎',
+          City.ilorin: '😎',
+          City.kano: '😎',
+          City.lagos: '😎',
+          City.ogbomoso: '😎',
+          City.osogbo: '😎',
+          City.oyo: '😎',
+          City.sokoto: '😎',
+        }[city] ??
+        '?',
+  );
+}
+
+// creating a state provider to input data into the ui
+final currentCityProvider = StateProvider<City?>((ref) => null);
+
+final weatherProvider = FutureProvider<WeatherEmoji?>((ref) {
+  final city = ref.watch(currentCityProvider);
+  if (city != null) {
+    return getWeather(city);
+  } else {
+    return unKnownWeatherEmoji;
+  }
+});
+
+enum City {
+  sokoto,
+  kano,
+  lagos,
+  abuja,
+  ife,
+  ibadan,
+  oyo,
+  ogbomoso,
+  osogbo,
+  ilesa,
+  ilorin
 }
